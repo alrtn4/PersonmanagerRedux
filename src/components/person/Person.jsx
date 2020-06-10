65b35e97a488f682e07fcdbdbb0f89dcc6944b61
alrtn4 <45690266+alrtn4@users.noreply.github.com>
2020-06-10 16:51:32 +0430

    chapter 8 file 4

--- src/components/person/Person.jsx
+++ src/components/person/Person.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import './person.css' ;
+// import './person.css' ;
 
 const Person = ({fullName , deleted , change}) => {
     return ( 
-        <div className='person'>
-            <p> {fullName} </p>
-            <input type='text' onChange={change} placeholder={fullName}/>
-            <button onClick={deleted}>حذف</button>
+        <div className='card text-white bg-info w-25 mb-3 mt-3 mx-auto'>
+            <div className='card-body text-center'>
+                <p className='d-block'> {fullName} </p>
+                <div className='input-group justify-content-center'>
+                    <input 
+                        className='form-control w-50' 
+                        type='text' 
+                        onChange={change} 
+                        placeholder={fullName}
+                    />
+                    <div className='input-group-prepend'>
+                        <butto 
+                            className='btn btn-sm btn-danger fa fa-trash' 
+                            onClick={deleted} 
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
