b769ec62b2276ba0d01e82eb22065101d086f04a
alrtn4 <45690266+alrtn4@users.noreply.github.com>
2020-10-20 15:15:06 +0330

    Updates

---
+++ precache-manifest.c5d90b1bbb07974b0c2829594a38ac06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6d1a715b25136ddff390afbbc8d0c4",
+    "url": "/PersonmanagerRedux/index.html"
+  },
+  {
+    "revision": "925b2ad3df5123c416cb",
+    "url": "/PersonmanagerRedux/static/css/2.49b532b6.chunk.css"
+  },
+  {
+    "revision": "1ab29c5c83e524bb3f7a",
+    "url": "/PersonmanagerRedux/static/css/main.897d10c4.chunk.css"
+  },
+  {
+    "revision": "925b2ad3df5123c416cb",
+    "url": "/PersonmanagerRedux/static/js/2.8f72cbdb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PersonmanagerRedux/static/js/2.8f72cbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab29c5c83e524bb3f7a",
+    "url": "/PersonmanagerRedux/static/js/main.81d4cf34.chunk.js"
+  },
+  {
+    "revision": "92feb54700f58d8cefbf",
+    "url": "/PersonmanagerRedux/static/js/runtime-main.bd5f8aed.js"
+  }
+]);
